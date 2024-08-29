49d0aabb9731fd3aa0eaa738ef938a92aeb14cab
Dan Azevedo <159485887+Azevedod1@users.noreply.github.com>
2024-08-29 17:30:12 -0300

    novo código adicionado

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,11 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+# Variável que armazena uma string
+minha_string = "Olá, mundo!"
+
+# Lista que armazena várias strings
+minha_lista = ["maçã", "banana", "laranja"]
+
+# Exibindo os valores
+print(minha_string)
+print(minha_lista)
